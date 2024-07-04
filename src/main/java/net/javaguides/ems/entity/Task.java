7c8f5c9b926c5e3ed1653e1c0d9a41c53f086923
Thirumalmani <thirumalmani@eluminaelearning.com.au>
2024-07-04 11:10:26 +0530

    task

---
+++ src/main/java/net/javaguides/ems/entity/Task.java
@@ -0,0 +1,23 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // this is the primary key which will be auto generated
+    private Long id;
+    private String task;
+    private boolean completed;
+
+}
